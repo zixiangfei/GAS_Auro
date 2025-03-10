36f66dbbedf1a69514955e52fa26a1135751649d
zixiangfei <zhengluzhou@foxmail.com>
2025-03-10 23:24:45 +0800

    The Base Character Class

---
+++ Source/GAS_Auro/GAS_Auro.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GAS_Auro.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAS_Auro, "GAS_Auro" );
